0b2405908d5206058f5cecaa18363cea1f3fbff5
Dillon Isaacs <isxbot@gmail.com>
2020-02-10 15:48:11 -0600

    Completion of DFACheck, added two test files in input/

--- DFACheck/src/DFACheck.java
+++ DFACheck/src/DFACheck.java
@@ -7,83 +7,110 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DFACheck {
-	public static void main(String[] args) {
-		ArrayList<String> states = new ArrayList<String>();
-		ArrayList<String> finalStates = new ArrayList<String>();
-		ArrayList<String> alphabet = new ArrayList<String>();
-		HashMap<String, HashMap<String, String>> deltas = new HashMap<>();
-		ArrayList<String> dfaSpec = new ArrayList<String>();
-		ArrayList<String> testStrings = new ArrayList<String>();
-		String currentState = null;
+    public static void main(String[] args) {
+        ArrayList<String> states = new ArrayList<String>();
+        ArrayList<String> finalStates = new ArrayList<String>();
+        HashMap<String, HashMap<String, String>> deltas = new HashMap<>();
+        ArrayList<String> dfaSpec = new ArrayList<String>();
+        ArrayList<String> testStrings = new ArrayList<String>();
+        String startState = null;
+        String currentState = null;
 
-		// Parse first file for DFA rules.
-		try (Stream<String> stream = Files.lines(Paths.get(args[0]))) {
-			dfaSpec = (ArrayList<String>) stream.collect(Collectors.toList());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        if(args.length == 0) {
+            System.err.println("DFACheck: no input files specified");
+        } else if(args.length == 1) {
+            System.err.println("DFACheck: invalid usage - the program must be given two files as input");
+        }
 
-		// Parse input from second file.
-		try (Stream<String> stream = Files.lines(Paths.get(args[1]))) {
-			testStrings = (ArrayList<String>) stream.collect(Collectors.toList());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        // Get the DFA characteristics from the first file.
+        try (Stream<String> stream = Files.lines(Paths.get(args[0]))) {
+            dfaSpec = (ArrayList<String>) stream.collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("DFACheck: the file \'" + args[0] + "\' could not be opened.");
+            System.exit(0);
+        }
 
-		currentState = dfaSpec.get(dfaSpec.indexOf("% Q0") + 1);
-		parseStates(dfaSpec, states);
-		parseFinalStates(dfaSpec, finalStates);
-		parseAlphabet(dfaSpec, alphabet);
+        // Parse test strings from second file.
+        try (Stream<String> stream = Files.lines(Paths.get(args[1]))) {
+            testStrings = (ArrayList<String>) stream.collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("DFACheck: the file \'" + args[0] + "\' could not be opened.");
+            System.exit(0);
+        }
 
-		// Map the deltas
-		deltas = buildDelta(states, dfaSpec);
+        // Get the states of the DFA; Q.
+        parseStates(dfaSpec, states);
 
-	}
+        // Get the final states of the DFA; F.
+        parseFinalStates(dfaSpec, finalStates);
 
-	/*
-	 * Parse states
-	 */
-	static void parseStates(ArrayList<String> dfaSpec, ArrayList<String> states) {
-		// Add states between %Q and next % Sigma.
-		for (int i = dfaSpec.indexOf("% Q") + 1; i < dfaSpec.indexOf("% Sigma"); i++) {
-			states.add(dfaSpec.get(i));
-		}
-	}
+        // Map the transition function.
+        deltas = buildDelta(states, dfaSpec);
+        
+        // Get the first current state; Q0.
+        startState = dfaSpec.get(dfaSpec.indexOf("% Q0") + 1);
 
-	/*
-	 * Parse finalStates
-	 */
-	static void parseFinalStates(ArrayList<String> dfaSpec, ArrayList<String> finalStates) {
-		for (int i = dfaSpec.indexOf("% F") + 1; i < dfaSpec.indexOf("% Q0"); i++) {
-			finalStates.add(dfaSpec.get(i));
-		}
-	}
+        // Test strings from the second file.
+        for (String line : testStrings) {
+            if (finalStates.contains(testLine(line, startState, deltas))) {
+                System.out.println(line + " accepted");
+            } else {
+                System.out.println(line + " rejected");
+            }
+        }
 
-	/*
-	 * Parse alphabet
-	 */
-	static void parseAlphabet(ArrayList<String> dfaSpec, ArrayList<String> alphabet) {
-		for (int i = dfaSpec.indexOf("% Sigma") + 1; i < dfaSpec.indexOf("% F"); i++) {
-			alphabet.add(dfaSpec.get(i));
-		}
-	}
+    }
 
-	/*
-	 * Build deltas
-	 */
-	static HashMap<String, HashMap<String, String>> buildDelta(ArrayList<String> states, ArrayList<String> dfaSpec) {
-		HashMap<String, HashMap<String, String>> deltas = new HashMap<>();
-		for (int i = dfaSpec.indexOf("% Delta") + 1; i < dfaSpec.size(); i++) {
-			String[] line = dfaSpec.get(i).split("\\s");
-			HashMap<String, String> transition = new HashMap<>();
-			transition.put(line[1], line[2]);
+    /*
+     * Check if the string is accepted by the DFA.
+     */
+    static String testLine(String line, String startState, HashMap<String, HashMap<String, String>> deltas) {
+        String[] lineArr = line.split("");
+        String currentState = startState;
+        for (int i = 0; i < lineArr.length; i++) {
+            currentState = deltas.get(currentState).get(lineArr[i]);
+        }
+        return currentState;
+    }
 
-			if (deltas.get(line[0]) == null) {
-				deltas.put(line[0], transition);
-			} else {
-				deltas.get(line[0]).put(line[1], line[2]);
-			}
-		}
-		return deltas;
-	}
+    /*
+     * Parse states
+     */
+    static void parseStates(ArrayList<String> dfaSpec, ArrayList<String> states) {
+        // Add states between %Q and next % Sigma.
+        for (int i = dfaSpec.indexOf("% Q") + 1; i < dfaSpec.indexOf("% Sigma"); i++) {
+            states.add(dfaSpec.get(i));
+        }
+    }
+
+    /*
+     * Parse finalStates
+     */
+    static void parseFinalStates(ArrayList<String> dfaSpec, ArrayList<String> finalStates) {
+        for (int i = dfaSpec.indexOf("% F") + 1; i < dfaSpec.indexOf("% Q0"); i++) {
+            finalStates.add(dfaSpec.get(i));
+        }
+    }
+
+    /*
+     * Map the transition function
+     */
+    static HashMap<String, HashMap<String, String>> buildDelta(ArrayList<String> states, ArrayList<String> dfaSpec) {
+        // Key is the state as String, value is HashMap of <input value, next state>.
+        HashMap<String, HashMap<String, String>> deltas = new HashMap<>();
+
+        for (int i = dfaSpec.indexOf("% Delta") + 1; i < dfaSpec.size(); i++) {
+            // [0] is current state, [1] is input value, [2] is next state.
+            String[] line = dfaSpec.get(i).split("\\s");
+            HashMap<String, String> transition = new HashMap<>();
+            transition.put(line[1], line[2]);
+
+            if (deltas.get(line[0]) == null) {
+                deltas.put(line[0], transition);
+            } else {
+                deltas.get(line[0]).put(line[1], line[2]);
+            }
+        }
+        return deltas;
+    }
 }
